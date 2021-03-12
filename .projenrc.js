36c7e29ca64652ae874aa76e888e4c5fadf2a8cc
Martin Mueller <damadden88@googlemail.com>
2021-03-12 07:17:10 +0100

    update cdk

--- .projenrc.js
+++ .projenrc.js
@@ -3,8 +3,7 @@ const { AwsCdkTypeScriptApp, NodePackageManager } = require('projen');
 const project = new AwsCdkTypeScriptApp({
   authorAddress: 'damadden88@googlemail.de',
   authorName: 'martin.mueller',
-  cdkVersion: '1.92.0',
-  cdkVersionPinning: true,
+  cdkVersion: '1.93.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
   name: 'aws-cdk-todolist',
