20a40c0e1d69993e0239d04edfc0822769b4f55e
Martin Mueller <damadden88@googlemail.com>
2021-04-16 15:45:03 +0200

    remove stage

--- .projenrc.js
+++ .projenrc.js
@@ -4,6 +4,7 @@ const project = new AwsCdkTypeScriptApp({
   authorAddress: 'damadden88@googlemail.de',
   authorName: 'martin.mueller',
   cdkVersion: '1.93.0',
+  cdkVersionPinning: true,
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
   name: 'aws-cdk-todolist',
@@ -12,9 +13,6 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-dynamodb',
     '@aws-cdk/aws-cognito',
     '@aws-cdk/aws-iam',
-  ],
-  deps: [
-    'aws-cdk-staging-pipeline',
   ],
   context: {
     '@aws-cdk/core:enableStackNameDuplicates': true,
--- src/appsync-stack.ts
+++ src/appsync-stack.ts
@@ -3,13 +3,12 @@ import * as cognito from '@aws-cdk/aws-cognito';
 import * as db from '@aws-cdk/aws-dynamodb';
 import * as iam from '@aws-cdk/aws-iam';
 import * as core from '@aws-cdk/core';
-import { CustomStack } from 'aws-cdk-staging-pipeline/lib/custom-stack';
 
 export interface AppSyncStackProps extends core.StackProps {
   readonly stage: string;
 }
 
-export class AppSyncStack extends CustomStack {
+export class AppSyncStack extends core.Stack {
   constructor(scope: core.Construct, id: string, props: AppSyncStackProps) {
     super(scope, id, props);
 
@@ -132,11 +131,10 @@ export class AppSyncStack extends CustomStack {
     });
 
     // Outputs
-    const graphql = new core.CfnOutput(this, 'appsyncGraphQLEndpointOutput', {
+    new core.CfnOutput(this, 'appsyncGraphQLEndpointOutput', {
       description: 'GraphQL Endpoint',
       value: graphQlApi.graphqlUrl,
     });
-    this.cfnOutputs.appsyncGraphQLEndpointOutput = graphql;
 
     new core.CfnOutput(this, 'awsUserPoolId', {
       description: 'userPoolID value for amplify exports',
