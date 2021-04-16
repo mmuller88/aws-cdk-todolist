410e3c63bb99e47d5b567c89a077302ffe4ba404
Martin Mueller <damadden88@googlemail.com>
2021-04-16 15:51:23 +0200

    add pinning

--- .projenrc.js
+++ .projenrc.js
@@ -4,6 +4,7 @@ const project = new AwsCdkTypeScriptApp({
   authorAddress: 'damadden88@googlemail.de',
   authorName: 'martin.mueller',
   cdkVersion: '1.93.0',
+  cdkVersionPinning: true,
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
   name: 'aws-cdk-todolist',
